2cbe7377caab65bc7fc6dfeb52a93b58f0c02311
Nikita Bikmaev <nikitabikmaev@gmail.com>
2020-03-29 12:07:46 +0300

    Fix world size

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/DiscoreMe/magic-world/entity"
+	"github.com/DiscoreMe/magic-world/world"
+	"time"
+)
 
+const worldWidth, worldHeight = 30, 15
+
+func main() {
+	w := world.NewWorld(worldWidth, worldHeight)
+	w.CreateLand()
+	hero := entity.NewHero("Nikita")
+	w.AddEntity(worldWidth/2, worldHeight/2, hero)
+
+	for {
+		w.Step()
+		if err := w.ExportToJSON("test.world"); err != nil {
+			panic(err)
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
